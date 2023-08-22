85906bd11ddcddd259ac8a6b8890769a6623ba06
AndiiSiryi <130078717+AndiiSiryi@users.noreply.github.com>
2023-08-22 15:27:41 +0300

    Завдання 1 - бібліотека SimpleLightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryList = document.querySelector('.gallery');
+galleryList.style.listStyle = "none";
 
+// Створення і рендер розмітки на підставі масиву даних galleryItems
+function createGalleryItem({preview, original, description}) {
+    const galleryItem = `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+          />
+        </a>
+      </li>`;
+    return galleryItem
+}
+const galleryItemsMarkup = galleryItems.map(createGalleryItem).join('');
+galleryList.insertAdjacentHTML('beforeend', galleryItemsMarkup)
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'outside',
+    captionDelay: 250,
+});
 console.log(galleryItems);
+
