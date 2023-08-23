e7015a2bdaa7c8af40fc51c62e2a1ee0adf64c09
AndiiSiryi <130078717+AndiiSiryi@users.noreply.github.com>
2023-08-23 09:07:12 +0300

    Завдання 2 - відеоплеєр

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import VimeoPlayer from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new VimeoPlayer(iframe);
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+
+if (savedTime) {
+  player.setCurrentTime(parseFloat(savedTime));
+}
+
+player.on('timeupdate', throttle(function(data) {     
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}, 1000));
+
+
 
