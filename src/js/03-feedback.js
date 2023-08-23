2cd0caa176848181f80f44649835bef2f3f539d7
AndiiSiryi <130078717+AndiiSiryi@users.noreply.github.com>
2023-08-23 20:15:23 +0300

    Завдання 3 - форма зворотного зв'язку

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageTextarea = form.querySelector('textarea[name="message"]');
+
+const savedState = JSON.parse(localStorage.getItem('feedback-form-state')) || {};
+
+if (savedState.email) {
+  emailInput.value = savedState.email;
+}
+if (savedState.message) {
+  messageTextarea.value = savedState.message;
+}
+
+form.addEventListener('input', throttle(function() {
+  const currentState = {
+    email: emailInput.value,
+    message: messageTextarea.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(currentState));
+}, 500));
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  localStorage.removeItem('feedback-form-state');
+  const feedbackData = {
+    email: emailInput.value,
+    message: messageTextarea.value,
+  };
+  console.log(feedbackData);
+  emailInput.value = '';
+  messageTextarea.value = '';
+});
